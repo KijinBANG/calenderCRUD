3b4ff3306efe5ee1d5defcc0469dfe34ad88507b
kijinBANG <mr.kijin.bang@gmail.com>
2022-03-07 16:26:08 +0900

    Fundamental setting

---
+++ src/main/java/com/keygenie/calendercrud/CalenderCrudApplication.java
@@ -0,0 +1,13 @@
+package com.keygenie.calendercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderCrudApplication.class, args);
+    }
+
+}
